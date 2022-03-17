6e1b702d0205b79acb2e4ee9fa032044878e6aa0
huynhvietve <86872128+huynhvietve@users.noreply.github.com>
2022-03-17 09:21:54 +0700

    them the b vao

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -14,10 +14,10 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        />
+        Learn React aaaa bbbb
       </header>
+      <p>con chim non</p>
     </div>
   );
 }
