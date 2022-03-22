c27bddafab68de9e0745ccbf63549bae8b3aadad
trung <trungs0132@gmail.com>
2022-03-22 15:19:50 +0700

    update

--- server/controllers/logincontroller.js
+++ server/controllers/logincontroller.js
@@ -3,13 +3,12 @@ const userModel = require("../models/loginModel");
 
 const loginController = {
   login: (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
+      const { username,password} = req.body
     if (!username) {
-      return res.status(400).json("you need to enter username");
+      return res.status(400).json(" You need to enter username ");
     }
     if (!password) {
-      return res.status(400).json("you need to enter password");
+      return res.status(400).json(" You need to enter password ");
     } else {
       const passwordMd5 = md5(req.body.password);
       userModel.login(username, passwordMd5, (response) => {
