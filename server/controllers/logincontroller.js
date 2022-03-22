a3a091aeacadfe259ac898416f7dce4403311c1b
trung <trungs0132@gmail.com>
2022-03-22 09:13:59 +0700

    login

--- server/controllers/logincontroller.js
+++ server/controllers/logincontroller.js
@@ -4,27 +4,19 @@ const md5 = require('md5');
 const loginController =  {
   login:  (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
     console.log(username, password)
-    const user = db.get('users').find({
-      username:  username
-    }).value();
-    if (!user) {
-        res.render('login', {
-            errors: ['User does not exits'],
-            values: res.body
-        });
-    }
-    console.log(md5(password));
-    if (md5(password) != user.password) {
-        res.render('login', {
-            errors: ['Wrong password'],
-            values: res.body
-        });
-    }
-    res.status(200).json(user);
-},
+    db.query('SELECT * FROM users WHERE username = ? and password = ?', [username, password],(errors,results) =>{
+        if(results.length > 0){
+            res.status(200).json('tài khoản và mật khẩu chính xác')
+            // res.redirect('internshipCourse')
+        }else{
+            // res.redirect('login')
+            res.status(400).json('sai tài khoản hoặc mật khẩu!')
 
+        }
+    })
+   
+},
 postLogout:(req, res, next) => {
     res.clearCookie("userId");
     res.redirect('/');
