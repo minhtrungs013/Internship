16df7b22e07a0091bffdc730c3604ad482bcb7e4
trung <trungs0132@gmail.com>
2022-03-23 14:07:59 +0700

    login7

--- server/controllers/logincontroller.js
+++ server/controllers/logincontroller.js
@@ -1,18 +1,30 @@
 const md5 = require("md5");
 const userModel = require("../models/userModel");
-
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 const loginController = {
   login: (req, res) => {
-      const { username,password} = req.body
+    const { username, password } = req.body;
     if (!username) {
       return res.status(400).json("You need to enter username");
     }
     if (!password) {
       return res.status(400).json("You need to enter password");
-    }{
+    }
+    {
       const passwordMd5 = md5(req.body.password);
-      userModel.login(username, passwordMd5, (response) => {
-        res.send({ token: response });
+      userModel.get({ username, passwordMd5 }, (response) => {
+        if (response.length > 0) {
+          const token = jwt.sign(
+            { idUser: response.idUser },
+            process.env.ACCESS_TOKEN_SECRET
+          );
+          return res.send({ token: token });
+        }
+        {
+          return res.status(400).json("Incorrect userName or password");
+        }
       });
     }
   },
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,29 +1,31 @@
 const connect = require("../config/db");
-const jwt = require("jsonwebtoken");
-const dotenv = require("dotenv");
-dotenv.config();
+
 const user = (users) => {
   this.idUser = users.idUser;
   this.userName = users.userName;
   this.password = users.password;
   this.email = users.email;
 };
-user.login = (username, password, results) => {
-  connect.query(
-    "SELECT * FROM users WHERE username = ? and password = ?",
-    [username, password],
-    (errors, users) => {
-      if (users.length > 0) {
-        const token = jwt.sign(
-          { idUser: users.idUser },
-          process.env.ACCESS_TOKEN_SECRET
-        );
-        return results(token);
-      } else{
-        return results("Incorrect userName or password",errors);
-      }
+
+user.get = (condition, res) => {
+  const query = buildQuery(condition);
+  connect.query(query, function (error, results) {
+    if (error) {
+      return res(error);
     }
-  );
+    return res(results);
+  });
+};
+
+const buildQuery = (params) => {
+  let stringQuery = "SELECT * FROM users WHERE 1 = 1";
+  if (params.username) {
+    stringQuery += " AND userName = '" + params.username + "'";
+  }
+  if (params.passwordMd5) {
+    stringQuery += " AND password = '" + params.passwordMd5 + "'";
+  }
+  return stringQuery;
 };
 
 module.exports = user;
