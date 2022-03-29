3cea90c5e1a82af5c8c3e2594c02b9e43007a621
lengochuy140799 <lengochuy789@gmail.com>
2022-03-29 15:36:29 +0700

    mentorUpdate

--- server/index.js
+++ server/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express();
 
-const loginRoute = require("./routers/login");
-const emailRoute = require("./routers/email");
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //ROUTES
-app.use("/", loginRoute);
-app.use("/", emailRoute);
+const mentorRouter = require("./routers/mentorRouter");
+app.use("/mentor", mentorRouter);
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log("App start success");
 });
